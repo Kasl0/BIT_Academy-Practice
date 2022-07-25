23bd5002cf76df4bb486f5cb2271af2e4e466fdd
Kasl0 <slonieckacper@gmail.com>
2022-07-25 18:50:14 +0200

    Completed Task 5

--- Pytest/Task5/test_A.py
+++ Pytest/Task5/test_A.py
@@ -6,3 +6,9 @@ from function import count_chars
 
 # Funkcja count_chars zwraca trzyelementową tablicę
 # która zawiera odpowiednio liczbę wystąpień znaku 'a', 'b' i 'c'
+
+@pytest.mark.parametrize("test_input, expected",
+[("abc", [1,1,1]), ("aaa", [3,0,0]), ("xyz", [0,0,0]), ("abccabcbabca", [4,4,4]), ("bbbbbbb", [0,7,0]), ("xytmunbz", [0,1,0])])
+
+def test(test_input, expected):
+    assert count_chars(test_input) == expected
